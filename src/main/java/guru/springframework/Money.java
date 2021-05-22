a7c4fd341fd6626ce2b68e84ac2a75704afc4549
Mahlori Ngobeni <mahlori.ngobeni@playsafesa.com>
2021-05-23 01:54:44 +0200

    More refectering

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,9 +3,19 @@ package guru.springframework;
 /**
  * Created by Mahlori Ngobeni on 2021/05/23
  */
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object object) {
