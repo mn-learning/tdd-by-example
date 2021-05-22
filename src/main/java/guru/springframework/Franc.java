07302f71f8638f4f980ec17d9c87cce88e585695
Mahlori Ngobeni <mahlori.ngobeni@playsafesa.com>
2021-05-23 01:27:11 +0200

    Dollar and Franc not equal

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -12,5 +12,4 @@ public class Franc extends Money {
     Franc times(int multiplier) {
         return new Franc(amount * multiplier);
     }
-
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -10,6 +10,7 @@ public class Money {
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount
+                && getClass().equals(object.getClass());
     }
 }
